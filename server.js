5f49d79d9fdd6268cdd1f57c32d4e81f88a517b9
Sreejith <sreejith.net@gmail.com>
2015-04-14 12:47:22 +0530

    Passport authenication

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 /**
  * Created by jaya on 7/4/15.
  */
-var express = require('express');
+var express = require('express'),
+    passport = require("passport"),
+    Auth = require('./server/authentication/authorization.js');
+
+
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
 
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
@@ -9,13 +17,35 @@ var config = require('./server/config/config')[env];
 //express
 var app = express();
 
-app.use(express.static(__dirname+'/public'));
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser.json()); // get information from html forms
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// required for passport
+app.use(session({ secret: 'mcqs3' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+
+require('./server/authentication/passport')(passport);
 
 require('./server/config/mongoose')(config);
 
 var questions = require('./server/controllers/questions');
-app.get('/api/questions', questions.getQuestions);
+
+app.get('/api/questions', passport.authenticate('local', { failureRedirect: '/login' }), questions.getQuestions);
+
+app.get('/api/isLoggedIn', Auth.isAuthenticated, function(req, res) {
+  res.json(req.user);
+});
+
+app.get('/logout', function (req, res) {
+    req.logout();
+    res.send(200);
+});
+
 
 app.listen(config.port,function(){
     console.log("Server Started on Port:"+config.port)
-})
+});
--- server/controllers/questions.js
+++ server/controllers/questions.js
@@ -4,6 +4,7 @@
 var question = require('mongoose').model('question');
 
 exports.getQuestions = function(req, res) {
+
     question.find({}).exec(function(err, collection) {
         res.send(collection);
     })
